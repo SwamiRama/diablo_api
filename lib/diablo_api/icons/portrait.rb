78b88076554b5b1c6e534e0a898c032d0adb376c
Jimmi <jimmi@rl-hosting.de>
2015-09-15 20:21:54 +0200

    fixed crusader bug

--- lib/diablo_api/icons/portrait.rb
+++ lib/diablo_api/icons/portrait.rb
@@ -4,15 +4,24 @@ module DiabloApi
       @convert = DiabloApi::Converter
 
       def self.small(hero_class, gender)
-        "http://media.blizzard.com/d3/icons/portraits/21/#{hero_class.delete('-')}_#{@convert.gender_number gender}.png"
+        "http://media.blizzard.com/d3/icons/portraits/21/#{edit_hero_class(hero_class)}_#{@convert.gender_number gender}.png"
       end
 
       def self.middle(hero_class, gender)
-        "http://media.blizzard.com/d3/icons/portraits/42/#{hero_class.delete('-')}_#{@convert.gender_number gender}.png"
+        "http://media.blizzard.com/d3/icons/portraits/42/#{edit_hero_class(hero_class)}_#{@convert.gender_number gender}.png"
       end
 
       def self.large(hero_class, gender)
-        "http://media.blizzard.com/d3/icons/portraits/64/#{hero_class.delete('-')}_#{@convert.gender_number gender}.png"
+        "http://media.blizzard.com/d3/icons/portraits/64/#{edit_hero_class(hero_class)}_#{@convert.gender_number gender}.png"
+      end
+
+      private
+      def self.edit_hero_class(hero_class)
+        if hero_class.include? 'crusader'
+          "x1_#{hero_class.delete('-')}"
+        else
+          hero_class.delete('-')
+        end
       end
     end
   end
--- spec/lib/diablo_api/icons/portrait_spec.rb
+++ spec/lib/diablo_api/icons/portrait_spec.rb
@@ -13,4 +13,7 @@ describe DiabloApi::Icons::Portrait do
   it 'get original monk female portrait' do
     expect(@portrait.large('monk', 1)).to eq 'http://media.blizzard.com/d3/icons/portraits/64/monk_female.png'
   end
+  it 'get original crusader female portrait' do
+    expect(@portrait.large('crusader', 1)).to eq 'http://media.blizzard.com/d3/icons/portraits/64/x1_crusader_female.png'
+  end
 end
