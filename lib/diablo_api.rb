be71eb014cf471539add4dcd0f500d349fd726aa
Jimmi <jimmi@rl-hosting.de>
2015-09-15 20:03:42 +0200

    fixed Icons bug

--- lib/diablo_api.rb
+++ lib/diablo_api.rb
@@ -4,6 +4,10 @@ require 'diablo_api/career'
 require 'diablo_api/hero'
 require 'diablo_api/item'
 require 'diablo_api/converter'
+require 'diablo_api/icons/item'
+require 'diablo_api/icons/paperdoll'
+require 'diablo_api/icons/portrait'
+require 'diablo_api/icons/skill'
 
 module DiabloApi
 
